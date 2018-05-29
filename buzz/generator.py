827dfc9a50f21329d7a41d758f824163a510217f
ahaagit <6714@student.jamk.fi>
2018-05-29 11:18:53 +0300

    Great chnages

--- buzz/generator.py
+++ buzz/generator.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous PROCESS IS GREAT', 'continuous integration',
+buzz = ('continuous TESTING IS GREAT !!!!', 'continuous integration',
     'continuous deployment', 'continuous improvement', 'devops')
 adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
 adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
